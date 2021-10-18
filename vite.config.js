9c225c965e005f38b8b847f93006651a12d41b3d
Dillon Kearns <dillon@dillonkearns.com>
2021-10-18 12:39:30 -0700

    Update dependencies and remove vite plugin for eslint.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
-import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [
-    elmPlugin({ debug: true }),
-    eslintPlugin({
-      include: ["index.ts"],
-    }),
-  ],
+  plugins: [elmPlugin({ debug: true })],
 });
