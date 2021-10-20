72a8d81ce67d616e5f013d5c41a38b71bf19ac9c
Dillon Kearns <dillon@dillonkearns.com>
2021-10-20 10:02:05 -0700

    Remove elm-ts-interop from config.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
-import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [elmPlugin({ debug: true })],
